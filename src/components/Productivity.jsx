1cd2eb698453f6b4efb65e184f1f46f8e772001c
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2025-01-11 18:34:53 +0530

    complete: productivity

--- src/components/Productivity.jsx
+++ src/components/Productivity.jsx
@@ -2,18 +2,42 @@ const Productivity = () => {
   return (
     <div className="w-full px-4 sm:px-10 2xl:px-80 2xl:pt-20 relative overflow-hidden py-20 lg:py-40">
       <div>
-        <h3 className=" text-3xl lg:text-6xl font-semibold">Unmatched productivity</h3>
+        <h3 className=" text-3xl lg:text-6xl font-semibold">
+          Unmatched productivity
+        </h3>
         <p className=" w-full lg:w-[50%] text-sm py-4 md:py-6 font-medium leading-tight">
           Huly is a process, project, time, and knowledge management platform
           that provides amazing collaboration opportunities for developers and
           product teams alike.
         </p>
       </div>
-      <div className="cards grid grid-cols-12 gap-4 ">
-        <div className=" col-span-12 sm:col-span-6 md:col-span-4 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg  bg-red-400">1</div>
-        <div className=" col-span-12 sm:col-span-6 md:col-span-8 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg bg-green-300">2</div>
-        <div className=" col-span-12 sm:col-span-6 md:col-span-8 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg bg-green-300"> 3</div>
-        <div className=" col-span-12 sm:col-span-6 md:col-span-4 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg bg-red-400">4</div>
+      <div className="cards grid grid-cols-12 gap-4 text-white">
+        <div className=" relative col-span-12 sm:col-span-6 md:col-span-4 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg  bg-center bg-cover bg-[url('../../public/keyboard.png')]">
+          <p className="absolute bottom-0 px-6 pb-4">
+            {" "}
+            <span className="font-bold">Keyboard shortcuts.</span> Work efficiently with instant
+            access to common actions.
+          </p>
+        </div>
+        <div className="relative  col-span-12 sm:col-span-6 md:col-span-8 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg   bg-center bg-cover bg-[url('../../public/teamplanner.png')]">
+           <p className="absolute bottom-0 px-6 pb-4 w-[60%]">
+            {" "}
+            <span className="font-bold">Team Planner. </span>Keep track of the bigger picture by viewing all individual tasks in one centralized team calendar.
+          </p>
+        </div>
+        <div className=" relative col-span-12 sm:col-span-6 md:col-span-8 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg  bg-center bg-cover bg-[url('../../public/time.png')]">
+          {" "}
+           <p className="absolute bottom-0 px-6 pb-4 w-[60%]">
+            {" "}
+            <span className="font-bold">Time-blocking. </span> Transform daily tasks into structured time blocks for focused productivity.
+          </p>
+        </div>
+        <div className=" relative col-span-12 sm:col-span-6 md:col-span-4 h-[270px] sm:h-[310px] md:h-[420px] rounded-lg bg-center bg-cover bg-black  bg-[url('../../public/notification.png')]">
+           <p className="absolute bottom-0 px-6 pb-4">
+            {" "}
+            <span className="font-bold">Notifications.</span> Keep up to date with any changes by receiving instant notifications.
+          </p>
+        </div>
       </div>
     </div>
   );
